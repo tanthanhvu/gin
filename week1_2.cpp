8e6665b74737d8a6a89a9e27b23b41f1e5e90253
tanthanhvu <79921409+tanthanhvu@users.noreply.github.com>
2021-05-20 17:02:33 +0700

    Delete week1_2.cpp

--- week1_2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    string name;
-    cout<<"Ban la?: ";
-    cin>>name;
-    cout<<"Chao mung ban "<< name <<" den voi the gioi lap trinh!!!!!!!!!!!!";
-    return 0;
-}
